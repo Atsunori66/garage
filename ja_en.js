84ea5627498d3c199d363815a51f6e6bfbcbf6d3
Atsunori66 <59758796+Atsunori66@users.noreply.github.com>
2020-10-08 03:08:33 +0900

    Add files via upload

--- ja_en.js
+++ ja_en.js
@@ -1,16 +1,20 @@
 javascript:(
     function(){
-        a='ja-jp';b='en-us';c='ja_jp';d='en_us';
+        a='ja-jp';b='en-us';
+        c='ja_jp';d='en_us';
+        e=document.location.pathname.substr(1,5); // MS docs 多言語用
         if(document.location.href.indexOf(a)>=0){
             location.href=document.location.href.replace(a,b);
         }else if(document.location.href.indexOf(b)>=0){
             location.href=document.location.href.replace(b,a);
         }else if(document.location.href.indexOf(c)>=0){
             location.href=document.location.href.replace(c,d);
-        }else{location.href=document.location.href.replace(d,c);
+        }else if(document.location.href.indexOf(d)>=0){
+            location.href=document.location.href.replace(d,c);
+        }else if(e!='en-us'){
+            location.href=document.location.href.replace(e,b);
         }
     }
 )(location.href);
 
-
-javascript:(function(){a='ja-jp';b='en-us';c='ja_jp';d='en_us';if(document.location.href.indexOf(a)>=0){location.href=document.location.href.replace(a,b);}else if(document.location.href.indexOf(b)>=0){location.href=document.location.href.replace(b,a);}else if(document.location.href.indexOf(c)>=0){location.href=document.location.href.replace(c,d);}else{location.href=document.location.href.replace(d,c);}})(location.href);
+javascript:(function(){a='ja-jp';b='en-us';c='ja_jp';d='en_us';e=document.location.pathname.substr(1,5);if(document.location.href.indexOf(a)>=0){location.href=document.location.href.replace(a,b);}else if(document.location.href.indexOf(b)>=0){location.href=document.location.href.replace(b,a);}else if(document.location.href.indexOf(c)>=0){location.href=document.location.href.replace(c,d);}else if(document.location.href.indexOf(d)>=0){location.href=document.location.href.replace(d,c);}else if(e!='en-us'){location.href=document.location.href.replace(e,b);}})(location.href);
