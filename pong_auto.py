ebb73bc068663a8c3fc4c51c46ce7c18c5489c8b
Atsunori66 <59758796+Atsunori66@users.noreply.github.com>
2020-11-01 00:30:48 +0900

    Add files via upload

---
+++ pong_auto.py
@@ -0,0 +1,207 @@
+import sys
+import math
+import pygame
+from random import randint, choice
+
+# ウィンドウの大きさを指定
+WIDTH = 700
+HEIGHT = 400
+
+# ボールの初期速度、直径、色（RGB）を指定
+INIT_SPEED = 11
+BALL_SIZE = 20
+BALL_COLOR = (255, 255, 255)
+
+# ラケットの長さを指定
+RACKET_LENGTH = 90
+
+# プレイヤーラケットの移動速度、位置、色を指定
+RACKET1_SIZE = (10, int((HEIGHT - RACKET_LENGTH) / 2), 10, RACKET_LENGTH)
+RACKET1_COLOR = (255, 255, 255)
+
+# COM ラケットの初期移動速度、位置、色を指定
+CPU_SWING = 8
+RACKET2_SIZE = (WIDTH - 20, int((HEIGHT - RACKET_LENGTH) / 2), 10, RACKET_LENGTH)
+RACKET2_COLOR = (255, 0, 0)
+
+# 外枠とセンターラインの位置を指定
+BORDER_TOP_SIZE = (0, 0, WIDTH, 5)
+BORDER_BOTTOM_SIZE = (0, HEIGHT - 5, WIDTH, 5)
+BORDER_CENTER_SIZE = (int(WIDTH / 2) - 2, 0, 4, HEIGHT)
+BORDER_LEFT_SIZE = (0, 0, 5, 600)
+BORDER_RIGHT_SIZE = (WIDTH - 5, 0, 5, HEIGHT)
+
+# 外枠の色を指定
+BORDER_COLOR = (255, 255, 255)
+
+# 以下ゲーム本体
+def main():
+    # 初期化、キー入力受付間隔、フレームレート取得、画面生成、タイトル生成
+    pygame.init()
+    pygame.key.set_repeat(10, 10)
+    clock = pygame.time.Clock()
+    surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Demo')
+
+    # ボールのスピードを初期速度に設定
+    speed = INIT_SPEED
+
+    # プレイヤー、COM の得点
+    score1 = 0
+    score2 = 0
+
+    # プレイヤー、COM の勝利フラグ
+    victory_1 = False
+    victory_2 = False
+
+    # スコア表示のフォントを指定
+    score_font = pygame.font.Font(None, 36)
+
+    # ゲーム終了時メッセージフォントを指定
+    end_font = pygame.font.Font(None, 50)
+
+    # プレイヤー勝利時メッセージ設定
+    message_win1 = end_font.render('You win!', True, (255, 255, 255))
+    message_pos1 = message_win1.get_rect()
+    message_pos1.centerx = int(WIDTH / 4)
+    message_pos1.centery = int(HEIGHT / 2)
+
+    # プレイヤー敗北時メッセージ設定
+    message_win2 = end_font.render('You lose...', True, (255, 255, 255))
+    message_pos2 = message_win2.get_rect()
+    message_pos2.centerx = int(WIDTH * 3 / 4)
+    message_pos2.centery = int(HEIGHT / 2)
+
+    # ボール、ラケットを生成
+    ball = pygame.Rect(surface.get_rect().centerx, surface.get_rect().centery, BALL_SIZE, BALL_SIZE)
+    racket1 = pygame.Rect(RACKET1_SIZE)
+    racket2 = pygame.Rect(RACKET2_SIZE)
+
+    # 外枠、センターラインを生成
+    border_top = pygame.Rect(BORDER_TOP_SIZE)
+    border_bottom = pygame.Rect(BORDER_BOTTOM_SIZE)
+    border_center = pygame.Rect(BORDER_CENTER_SIZE)
+    border_left = pygame.Rect(BORDER_LEFT_SIZE)
+    border_right = pygame.Rect(BORDER_RIGHT_SIZE)
+
+    # ボールの初期射出角度をランダムに指定（画面中央から真上、真下に射出されるのを防止）
+    direction = choice([randint(50, 70), randint(110, 130), randint(230, 250), randint(290, 310)])
+
+    # 以下ゲーム進行をループ
+    while True:
+
+        # プレイヤーのスコア表示を設定
+        score1_show = score_font.render('COM 1: ' + str(score1), True, (255, 255, 255))
+        score1_pos = score1_show.get_rect()
+        score1_pos.centerx = int(WIDTH / 4)
+        score1_pos.centery = 40
+
+        # COM のスコア表示を設定
+        score2_show = score_font.render('COM 2: ' + str(score2), True, (255, 255, 255))
+        score2_pos = score2_show.get_rect()
+        score2_pos.centerx = int(WIDTH * 3 / 4)
+        score2_pos.centery = 40
+
+        # Esc キーか画面の×ボタンで終了
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        # COM の挙動を設定
+        # COM ラケットがボールより上にあったら下に移動
+        # COM ラケットがボールより下にあったら上に移動
+        # ボールが遠くにあったら早く移動
+        if racket1.centery < ball.centery and racket1.bottom < HEIGHT - 5 and ball.centerx < int(WIDTH / 2):
+            racket1.centery += CPU_SWING
+            if racket1.centery < ball.centery - int(HEIGHT / 8) and ball.centerx < int(WIDTH / 2):
+                racket1.centery += CPU_SWING + randint(-3, 3)
+        elif racket1.centery > ball.centery and racket1.top > 5 and ball.centerx < int(WIDTH / 2):
+            racket1.centery -= CPU_SWING
+            if racket1.centery > ball.centery + int(HEIGHT / 8) and ball.centerx < int(WIDTH / 2):
+                racket1.centery -= CPU_SWING + randint(-3, 3)
+
+        if racket2.centery < ball.centery and racket2.bottom < HEIGHT - 5 and ball.centerx > int(WIDTH / 2):
+            racket2.centery += CPU_SWING
+            if racket2.centery < ball.centery - int(HEIGHT / 8) and ball.centerx > int(WIDTH / 2):
+                racket2.centery += CPU_SWING + randint(-3, 3)
+        elif racket2.centery > ball.centery and racket2.top > 5 and ball.centerx > int(WIDTH / 2):
+            racket2.centery -= CPU_SWING
+            if racket2.centery > ball.centery + int(HEIGHT / 8) and ball.centerx > int(WIDTH / 2):
+                racket2.centery -= CPU_SWING + randint(-3, 3)
+
+        # ラケットとボール衝突時、角度を反射（ランダムで誤差あり）、ボールを加速
+        if racket1.colliderect(ball):
+            direction = -(racket1.centery - ball.centery) + randint(-20, 20)
+            speed += 0.3
+        if racket2.colliderect(ball):
+            direction = 180 + (racket2.centery - ball.centery) + randint(-20, 20)
+            speed += 0.1
+
+        # ボールが上下の枠に当たった時、角度を反射、ボールを加速
+        if ball.centery < 0 or ball.centery > HEIGHT:
+            direction = -direction
+            speed += 0.2
+
+        # ボールが画面内にある時、ボールを移動
+        # プレイヤーか COM がボールを逸らした時、点数処理
+        # 片方が2点差以上を付け、11点以上を獲得した時、勝利フラグを立てる（卓球ルールを踏襲）
+        if ball.centerx >= 0 and ball.centerx <= WIDTH:
+            ball.centerx += int(math.cos(math.radians(direction)) * speed)
+            ball.centery += int(math.sin(math.radians(direction)) * speed)
+        else:
+            if (score1 < 11 and score2 < 11) or (score1 >=11 and score1 < score2 + 2) or (score2 >= 11 and score2 < score1 + 2):
+                if ball.centerx > WIDTH:
+                    score1 += 1
+                    ball.left = int(WIDTH / 2)
+                    ball.top = int(HEIGHT / 2)
+                    direction = choice([randint(50, 70), randint(110, 130), randint(230, 250), randint(290, 310)])
+                    speed = INIT_SPEED
+                if ball.centerx < 0:
+                    score2 += 1
+                    ball.left = int(WIDTH / 2)
+                    ball.top = int(HEIGHT / 2)
+                    direction = choice([randint(50, 70), randint(110, 130), randint(230, 250), randint(290, 310)])
+                    speed = INIT_SPEED
+            if score1 >= 11 and score1 >= score2 + 2:
+                victory_1 = True
+            if score2 >= 11 and score2 >= score1 + 2:
+                victory_2 = True
+
+        # 画面に色を描画（卓球台に寄せた）
+        surface.fill((0, 100, 200))
+
+        # プレイヤーか COM が勝利条件を満たしたとき、メッセージを画面に描画
+        if victory_1:
+            surface.blit(message_win1, (message_pos1))
+        if victory_2:
+            surface.blit(message_win2, (message_pos2))
+
+        # 外枠、センターラインを画面に描画
+        pygame.draw.rect(surface, BORDER_COLOR, border_top)
+        pygame.draw.rect(surface, BORDER_COLOR, border_bottom)
+        pygame.draw.rect(surface, BORDER_COLOR, border_center)
+        pygame.draw.rect(surface, BORDER_COLOR, border_left)
+        pygame.draw.rect(surface, BORDER_COLOR, border_right)
+
+        # スコアを画面に描画
+        surface.blit(score1_show, (score1_pos))
+        surface.blit(score2_show, (score2_pos))
+
+        # ラケットを画面に描画
+        pygame.draw.rect(surface, RACKET1_COLOR, racket1)
+        pygame.draw.rect(surface, RACKET2_COLOR, racket2)
+
+        # ボールを画面に描画
+        pygame.draw.ellipse(surface, BALL_COLOR, ball)
+
+        # 画面を更新、フレームレートの最大値を 60fps に設定
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
---
+++ pong_pvp.py
@@ -0,0 +1,160 @@
+import sys
+import math
+import pygame
+from random import randint, choice
+
+WIDTH = 700
+HEIGHT = 400
+
+INIT_SPEED = 8
+BALL_SIZE = 20
+BALL_COLOR = (255, 255, 255)
+
+SWING = 2
+RACKET1_SIZE = (10, 180, 10, 90)
+RACKET1_COLOR = (255, 255, 255)
+
+RACKET2_SIZE = (WIDTH - 20, 180, 10, 90)
+RACKET2_COLOR = (255, 255, 255)
+
+BORDER_TOP_SIZE = (0, 0, WIDTH, 5)
+BORDER_BOTTOM_SIZE = (0, HEIGHT - 5, WIDTH, 5)
+BORDER_LEFT_SIZE = (0, 0, 5, 600)
+BORDER_RIGHT_SIZE = (WIDTH - 5, 0, 5, HEIGHT)
+
+BORDER_COLOR = (255, 255, 255)
+
+def main():
+    pygame.init()
+    pygame.key.set_repeat(1, 1)
+    clock = pygame.time.Clock()
+    surface = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Pong PvP')
+
+    end_font = pygame.font.Font(None, 60)
+    message_over1 = end_font.render('Player 2 wins!', True, (255, 0, 255))
+    message_pos1 = message_over1.get_rect()
+    message_pos1.centerx = int(WIDTH / 2)
+    message_pos1.centery = int(HEIGHT / 2)
+
+    message_over2 = end_font.render('Player 1 wins!', True, (0, 255, 255))
+    message_pos2 = message_over2.get_rect()
+    message_pos2.centerx = int(WIDTH / 2)
+    message_pos2.centery = int(HEIGHT / 2)
+
+    ball = pygame.Rect(surface.get_rect().centerx, surface.get_rect().centery, BALL_SIZE, BALL_SIZE)
+    racket1 = pygame.Rect(RACKET1_SIZE)
+    racket2 = pygame.Rect(RACKET2_SIZE)
+
+    border_top = pygame.Rect(BORDER_TOP_SIZE)
+    border_bottom = pygame.Rect(BORDER_BOTTOM_SIZE)
+    border_left = pygame.Rect(BORDER_LEFT_SIZE)
+    border_right = pygame.Rect(BORDER_RIGHT_SIZE)
+
+    direction = choice([randint(50, 70), randint(110, 130), randint(230, 250), randint(290, 310)])
+
+    speed = INIT_SPEED
+
+    life1 = 5
+    life2 = 5
+
+    game_over_1 = False
+    game_over_2 = False
+
+    while True:
+        life_font = pygame.font.Font(None, 36)
+
+        life1_show = life_font.render('Player 1: ' + str(life1), True, (255, 255, 255))
+        life1_pos = life1_show.get_rect()
+        life1_pos.centerx = 240
+        life1_pos.centery = 40
+
+        life2_show = life_font.render('Player 2: ' + str(life2), True, (255, 255, 255))
+        life2_pos = life2_show.get_rect()
+        life2_pos.centerx = 460
+        life2_pos.centery = 40
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w and racket1.top > 15:
+                    racket1.centery -= SWING
+                if event.key == pygame.K_s and racket1.bottom < HEIGHT - 15:
+                    racket1.centery += SWING
+                if event.key == pygame.K_a and racket1.left > 10:
+                    racket1.centerx -= SWING
+                if event.key == pygame.K_d and racket1.right < 100:
+                    racket1.centerx += SWING
+                if event.key == pygame.K_UP and racket2.top > 15:
+                    racket2.centery -= SWING
+                if event.key == pygame.K_DOWN and racket2.bottom < HEIGHT - 15:
+                    racket2.centery += SWING
+                if event.key == pygame.K_LEFT and racket2.left > 600:
+                    racket2.centerx -= SWING
+                if event.key == pygame.K_RIGHT and racket2.right < WIDTH - 10:
+                    racket2.centerx += SWING
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+        if racket1.colliderect(ball):
+            direction = -(racket1.centery - ball.centery) + randint(-20, 20)
+            speed += 0.2
+        if racket2.colliderect(ball):
+            direction = 180 + (racket2.centery - ball.centery) + randint(-20, 20)
+            speed += 0.2
+
+        if ball.centery < 0 or ball.centery > HEIGHT:
+            direction = -direction
+            speed += 0.2
+        if ball.centerx >= 0 and ball.centerx <= WIDTH:
+            ball.centerx += int(math.cos(math.radians(direction)) * speed)
+            ball.centery += int(math.sin(math.radians(direction)) * speed)
+        else:
+            if life1 >= 1 and life2 >= 1:
+                if ball.centerx < 0:
+                    life1 -= 1
+                    ball.left = int(WIDTH / 2)
+                    ball.top = int(HEIGHT / 2)
+                    direction = choice([randint(60, 80), randint(100, 120), randint(240, 260), randint(280, 300)])
+                    speed = INIT_SPEED
+                if ball.centerx > WIDTH:
+                    life2 -= 1
+                    ball.left = int(WIDTH / 2)
+                    ball.top = int(HEIGHT / 2)
+                    direction = choice([randint(60, 80), randint(100, 120), randint(240, 260), randint(280, 300)])
+                    speed = INIT_SPEED
+
+            if life1 < 1:
+                game_over_1 = True
+            if life2 < 1:
+                game_over_2 = True
+
+        surface.fill('black')
+
+        if game_over_1:
+            surface.blit(message_over1, (message_pos1))
+        if game_over_2:
+            surface.blit(message_over2, (message_pos2))
+
+        pygame.draw.rect(surface, BORDER_COLOR, border_top)
+        pygame.draw.rect(surface, BORDER_COLOR, border_bottom)
+        pygame.draw.rect(surface, BORDER_COLOR, border_left)
+        pygame.draw.rect(surface, BORDER_COLOR, border_right)
+
+        pygame.draw.rect(surface, RACKET1_COLOR, racket1)
+        pygame.draw.rect(surface, RACKET2_COLOR, racket2)
+
+        pygame.draw.ellipse(surface, BALL_COLOR, ball)
+
+        surface.blit(life1_show, (life1_pos))
+        surface.blit(life2_show, (life2_pos))
+
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
