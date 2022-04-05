05509b57d802f3ab51284fdca585e06604e1a211
Atsunori66 <59758796+Atsunori66@users.noreply.github.com>
2022-04-06 01:35:26 +0900

    Add files via upload

---
+++ wordle.py
@@ -0,0 +1,29 @@
+guess = input('enter: ')
+
+answer = 'world'
+
+if guess[0] in answer:
+    if guess[0] == answer[0]:
+        print(guess[0], ': hit')
+    else:
+        print(guess[0], ': blow')
+if guess[1] in answer:
+    if guess[1] == answer[1]:
+        print(guess[1], ': hit')
+    else:
+        print(guess[1], ': blow')
+if guess[2] in answer:
+    if guess[2] == answer[2]:
+        print(guess[2], ': hit')
+    else:
+        print(guess[2], ': blow')
+if guess[3] in answer:
+    if guess[3] == answer[3]:
+        print(guess[3], ': hit')
+    else:
+        print(guess[3], ': blow')
+if guess[4] in answer:
+    if guess[4] == answer[4]:
+        print(guess[4], ': hit')
+    else:
+        print(guess[4], ': blow')
